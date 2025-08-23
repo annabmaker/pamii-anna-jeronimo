b1fd396b654db676daf8b34032523b51249e06f8
annabmaker <annabmaker@gmail.com>
2025-08-22 22:20:21 -0300

    Aula03 GlueStack

---
+++ aula03/app/index.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@/components/ui/box"
+import { Text } from "@/components/ui/text"
+
+export default function Index() {
+  return (
+    <Box className="bg-tertiary-400 p-10">
+    <Text className="text-typography-8">This is the Box</Text>
+  </Box>
+)
+}
