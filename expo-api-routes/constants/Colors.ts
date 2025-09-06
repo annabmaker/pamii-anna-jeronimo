4035015cf044f9c2197e538da6e75656f7ca6abe
annabmaker <annabmaker@gmail.com>
2025-09-06 00:29:44 -0300

    aula04 API-Routes

---
+++ expo-api-routes/constants/Colors.ts
@@ -0,0 +1,19 @@
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#fff';
+
+export default {
+  light: {
+    text: '#000000ff',
+    background: '#fff',
+    tint: tintColorLight,
+    tabIconDefault: '#080808ff',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    tint: tintColorDark,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorDark,
+  },
+};
